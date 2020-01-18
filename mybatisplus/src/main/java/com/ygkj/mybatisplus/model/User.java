d89490b26087c96936f85afaf01b09a6345fb03b
胡聪 <1073729668@qq.com>
2020-01-18 15:57:31 +0800

    mybatisplus 自动生成相应的文件

---
+++ mybatisplus/src/main/java/com/ygkj/mybatisplus/model/User.java
@@ -0,0 +1,57 @@
+package com.ygkj.mybatisplus.model;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.annotations.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hucong
+ * @since 2020-01-17
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private Integer age;
+    private String username;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+        ", id=" + id +
+        ", age=" + age +
+        ", username=" + username +
+        "}";
+    }
+}
---
+++ mybatisplus/src/main/java/com/ygkj/mybatisplus/service/UserService.java
@@ -0,0 +1,16 @@
+package com.ygkj.mybatisplus.service;
+
+import com.ygkj.mybatisplus.model.User;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author hucong
+ * @since 2020-01-17
+ */
+public interface UserService extends IService<User> {
+
+}
